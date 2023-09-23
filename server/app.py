49a22009e30e62d023d91e9fd7ad33392d10631a
DKabucho <danielkabucho2004@gmail.com>
2023-09-23 21:59:21 +0300

    complete assignment

--- server/app.py
+++ server/app.py
@@ -4,5 +4,19 @@ from flask import Flask
 
 app = Flask(__name__)
 
+@app.route('/')
+def index():
+    return '<h1>Python Operations with Flask Routing and Views</h1>'
+
+@app.route('/print/<string:text>')
+def print_string(text):
+    print(text)
+    return text
+    
+@app.route('/count/<int:number>')
+def count(number):
+    numbers = '\n'.join(str(i) for i in range(number + 1))
+    return numbers, 200
+
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
